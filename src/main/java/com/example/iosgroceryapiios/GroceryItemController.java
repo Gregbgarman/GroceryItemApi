92f6701b99f25a422659861216ee0a0d5f6c882b
Gregbgarman <gregory.b.garman@gmail.com>
2021-12-25 02:26:33 -0500

    edits

--- src/main/java/com/example/iosgroceryapiios/GroceryItemController.java
+++ src/main/java/com/example/iosgroceryapiios/GroceryItemController.java
@@ -12,19 +12,53 @@ import java.util.List;
 @RestController
 public class GroceryItemController {
 
-    //http://localhost:8080/allitems
+    //http://localhost:8080/groceryitems
     @GetMapping("/groceryitems")
-    public OuterClass GetItems()  {
-
+    public OuterClass GetItems() throws IOException  {
         List<GroceryItem> AllGroceryItems = new ArrayList<>();
         AddAllItems(AllGroceryItems);
         Result results=new Result("groceryitems",AllGroceryItems);
         return new OuterClass("OK", AllGroceryItems.size(), "Grocery Inventory for ios app",results,"Greg Garman");
+
     }
 
-    private void AddAllItems(List<GroceryItem> AllGroceryItems)  {
-          AllGroceryItems.add(new GroceryItem("Gatorade","https://cutpcdnwimages.azureedge.net/images/products/10000/012188-600x600-A.jpg","Orange"));
-           AllGroceryItems.add(new GroceryItem("Gatorade","https://cutpcdnwimages.azureedge.net/images/products/10000/012057-600x600-A.jpg","Lemon Lime"));
+    private void AddAllItems(List<GroceryItem> AllGroceryItems) throws IOException  {
+
+        Reader in = new BufferedReader(new FileReader("src/main/java/com/example/iosgroceryapiios/GroceryDetails.txt"));
+        int inread;
+        String str = "";
+        int counter=1;
+        StringBuilder ItemName=new StringBuilder();
+        StringBuilder Itemurl=new StringBuilder();
+        String Desc=new String();
+
+        while ((inread=in.read()) !=-1){
+            if ( (char)inread=='\n'){
+
+                if (counter==1){
+                    ItemName.append(str);
+                    counter++;
+                }
+
+                else if (counter==2){
+                    Itemurl.append(str);
+                    counter++;
+                }
+
+                else if (counter==3){
+                    AllGroceryItems.add(new GroceryItem(ItemName.toString(),Itemurl.toString(),str));
+                    ItemName.setLength(0);
+                    Itemurl.setLength(0);
+                    counter=1;
+                }
+                str="";
+
+            }
+            else{
+                str+=(char)inread;
+            }
+
+        }
 
     }
 
